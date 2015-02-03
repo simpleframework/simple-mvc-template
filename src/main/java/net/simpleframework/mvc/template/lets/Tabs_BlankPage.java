77670260e20cac871d588e86da2da3e834fe28fe
CHENKAN <CHENKAN@CHENKAN-PC>
2015-02-03 16:07:17 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/mvc/template/lets/Tabs_BlankPage.java
+++ src/main/java/net/simpleframework/mvc/template/lets/Tabs_BlankPage.java
@@ -1,5 +1,8 @@
 package net.simpleframework.mvc.template.lets;
 
+import net.simpleframework.mvc.PageParameter;
+import net.simpleframework.mvc.common.element.ElementList;
+import net.simpleframework.mvc.common.element.TabButtons;
 import net.simpleframework.mvc.template.AbstractTemplatePage;
 
 /**
@@ -9,4 +12,32 @@ import net.simpleframework.mvc.template.AbstractTemplatePage;
  *         http://www.simpleframework.net
  */
 public abstract class Tabs_BlankPage extends AbstractTemplatePage {
+
+	@Override
+	protected void onForward(final PageParameter pp) {
+		super.onForward(pp);
+		pp.addImportCSS(AbstractTemplatePage.class, "/tabs.css");
+	}
+
+	public TabButtons getTabButtons(final PageParameter pp) {
+		return null;
+	}
+
+	public String toTabsbarHTML(final PageParameter pp) {
+		final StringBuilder sb = new StringBuilder();
+		final ElementList el = getLeftElements(pp);
+		if (el != null && el.size() > 0) {
+			sb.append("<div class='le'>").append(el).append("</div>");
+		}
+		final TabButtons tabs = getTabButtons(pp);
+		if (tabs != null && tabs.size() > 0) {
+			sb.append("<div class='re'>").append(tabs.toString(pp)).append("</div>");
+		}
+		return sb.toString();
+	}
+
+	public String toContentHTML(final PageParameter pp) {
+		final StringBuilder sb = new StringBuilder();
+		return sb.toString();
+	}
 }
