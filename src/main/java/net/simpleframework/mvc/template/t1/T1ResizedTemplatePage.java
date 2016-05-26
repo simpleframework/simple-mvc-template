4c507aa20b2155c8ad5fa8c064a5a55d5b68662a
CHENK <CHENK@CHENK-PC>
2016-05-26 20:18:25 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/mvc/template/t1/T1ResizedTemplatePage.java
+++ src/main/java/net/simpleframework/mvc/template/t1/T1ResizedTemplatePage.java
@@ -94,7 +94,7 @@ public abstract class T1ResizedTemplatePage extends T1TemplatePage implements IP
 		if (Convert.toBool(getVariables(pp).get("headerHidden"))) {
 			sb.append(" style='display:none;'");
 		}
-		sb.append(">").append(getMVELNamedTemplate("header")).append("</div>");
+		sb.append(">").append(getMVELNamedTemplate(pp, "header")).append("</div>");
 		return sb.toString();
 	}
 
